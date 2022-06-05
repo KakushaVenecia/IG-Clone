cdc02aadc913994f0e5008b999399429a407ce98
KakushaVenecia <kakushavenecia@gmail.com>
2022-06-05 22:18:33 +0300

    This will add the initial models into the admin

---
+++ instagram/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+# from models import Image, Stream, Follow
+
+# # Register your models here.
+# admin.site.register(Image)
+# admin.site.register(Stream)
+# admin.site.register(Follow)
